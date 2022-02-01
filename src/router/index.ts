9e594ac8c76790fa18e7d30b9875cc3aff537f5f
eyzi <contact@eyzi.dev>
2022-02-01 00:22:34 -0500

    feat(page): add 404 catch all

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import NotFound from "../views/404.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/404",
+    name: "NotFound",
+    component: NotFound,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404",
   },
 ];
 
---
+++ src/views/404.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="body">
+    <div>
+      <div id="message">Page Not Found</div>
+      <div id="cta"><a href="/">&lt; Back to home</a></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NotFound",
+  created: () => {
+    document.title = "Eyzi | 404";
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 100vw;
+  min-height: 100vh;
+}
+
+#message {
+  font-size: 1.5em;
+  padding: 0.5em;
+}
+</style>
