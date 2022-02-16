b384f4be8ad5b5cdaefd9488d1bc8265acf42244
eyzi <contact@eyzi.dev>
2022-02-15 23:50:35 -0500

    feat(manifest): add vue config file

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      name: "Eyzi Website",
+      short_name: "Eyzi Website",
+      theme_color: "#ff4d4d",
+      icons: [
+        {
+          src: `/favicon.ico`,
+          type: "image/x-icon",
+        },
+      ],
+    },
+  },
+};
